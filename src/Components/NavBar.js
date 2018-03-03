fbce90ba3cd104a404b0df27df17b4a967c315f7
ikantspelgud <ikantspelgud@gmail.com>
2018-03-02 23:32:31 -0600

    fixed some formating with nav bar column

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -19,36 +19,31 @@ class NavBar extends Component {
 
   render(){
     return (
-    	<div>
-        <div className="header">
-
-        <Grid>
-          <Row>
-              <Col xs={6}>
-                <div className="temp-photo"/>
+    	<div className="header">
+        <Grid className="v-fill">
+          <Row className="v-fill">
+              <Col xs={0} md={1}/>
+              <Col xs={12} md={3}>
+                <div className="temp-photo v-fill"/>
               </Col>
-              <Col xs={6}>
-                <ButtonToolbar>
-                    <ButtonGroup>
-                      <Button bsStyle="link" bsSize="large" className="top-link">
-                          Bio
-                      </Button>
-                      <Button bsStyle="link" bsSize="large" className="top-link">
-                          Past Jobs
-                      </Button>
-                      <Button bsStyle="link" bsSize="large" className="top-link">
-                          Resume
-                      </Button>
-                      <Button bsStyle="link" bsSize="large" className="top-link">
-                          Contact
-                      </Button>
-                    </ButtonGroup>
-                </ButtonToolbar>
+              <Col xs={12} md={8}>
+                <ButtonGroup>
+                  <Button bsStyle="link" bsSize="large" className="top-link">
+                      Bio
+                  </Button>
+                  <Button bsStyle="link" bsSize="large" className="top-link">
+                      Past Jobs
+                  </Button>
+                  <Button bsStyle="link" bsSize="large" className="top-link">
+                      Resume
+                  </Button>
+                  <Button bsStyle="link" bsSize="large" className="top-link">
+                      Contact
+                  </Button>
+                </ButtonGroup>
               </Col>
           </Row>
         </Grid>    		  
-            </div>
-
     	</div>
     )
   }
